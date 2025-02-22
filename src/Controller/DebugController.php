8ff7dbb90ef007923bd776d8d1cbb30779603768
Alexander Kharchenko <morontt@yandex.ru>
2025-02-22 11:48:01 +0300

    Add form for updates data

---
+++ src/Controller/DebugController.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * User: morontt
+ * Date: 22.02.2025
+ * Time: 10:17
+ */
+
+namespace TeleBot\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use TeleBot\Form\UpdatesForm;
+use Xelbot\Telegram\Robot;
+
+class DebugController extends AbstractController
+{
+    public function updatesFormAction(Request $request, Robot $bot): Response
+    {
+        $form = $this->createForm(UpdatesForm::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $updates = json_decode(
+                $data['data'],
+                true,
+                512,
+                JSON_BIGINT_AS_STRING | JSON_INVALID_UTF8_SUBSTITUTE | JSON_THROW_ON_ERROR
+            );
+            if (!is_array($updates)) {
+                return new Response(Response::$statusTexts[Response::HTTP_BAD_REQUEST], Response::HTTP_BAD_REQUEST);
+            }
+
+            $bot->handle($updates);
+        }
+
+        return $this->render('debug/updatesForm.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
---
+++ src/Form/UpdatesForm.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * User: morontt
+ * Date: 22.02.2025
+ * Time: 10:44
+ */
+
+namespace TeleBot\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class UpdatesForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('data', TextareaType::class, [
+                'required' => true,
+                'label' => 'JSON',
+                'attr' => [
+                    'rows' => 10,
+                    'cols' => 50,
+                ],
+            ])
+            ->add('submit', SubmitType::class)
+        ;
+    }
+}
