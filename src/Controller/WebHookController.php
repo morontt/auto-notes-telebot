b7c182fe4b3518c3dc1b490e9f5361a1ba2499c4
Alexander Kharchenko <morontt@yandex.ru>
2025-02-27 10:08:56 +0300

    WebHookController

---
+++ src/Controller/WebHookController.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * User: morontt
+ * Date: 27.02.2025
+ * Time: 09:23
+ */
+
+namespace TeleBot\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Xelbot\Telegram\Robot;
+
+class WebHookController extends AbstractController
+{
+    /**
+     * @var Robot
+     */
+    private Robot $telegramBot;
+
+    /**
+     * @param Robot $telegramBot
+     * @param string $secretToken
+     */
+    public function __construct(Robot $telegramBot, string $secretToken)
+    {
+        $this->telegramBot = $telegramBot;
+        $this->secretToken = $secretToken;
+    }
+
+    #[Route('/webhook/{token}', methods: ['POST'])]
+    public function indexAction(Request $request, string $token): Response
+    {
+        if (!hash_equals(sha1($this->secretToken), sha1($token))) {
+            return new Response("Get out!\n", 403, ['content-type' => 'text/plain']);
+        }
+
+        $this->telegramBot->handle($request->request->all());
+
+        return new Response("ok\n", 200, ['content-type' => 'text/plain']);
+    }
+}
---
+++ src/EventListener/JsonBodyListener.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * User: morontt
+ * Date: 27.02.2025
+ * Time: 09:43
+ */
+
+namespace TeleBot\EventListener;
+
+use JsonException;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+class JsonBodyListener
+{
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param RequestEvent $event
+     *
+     * @throws BadRequestHttpException
+     */
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+        $method = $request->getMethod();
+
+        if (in_array($method, ['POST', 'PUT', 'DELETE'])
+            && !count($request->request->all())
+        ) {
+            $contentType = $request->headers->get('Content-Type');
+
+            $format = null === $contentType
+                ? $request->getRequestFormat()
+                : $request->getFormat($contentType);
+
+            if ($format === 'json') {
+                $content = $request->getContent();
+                if (!empty($content)) {
+                    try {
+                        $data = json_decode(
+                            $content,
+                            true,
+                            512,
+                            JSON_BIGINT_AS_STRING | JSON_INVALID_UTF8_SUBSTITUTE | JSON_THROW_ON_ERROR
+                        );
+
+                        if (is_array($data)) {
+                            $request->request = new ParameterBag($data);
+
+                            return;
+                        }
+                    } catch (JsonException $e) {
+                        $this->logger->error('JsonException', ['exception' => $e]);
+                    }
+
+                    throw new BadRequestHttpException('Invalid ' . $format . ' message received');
+                }
+            }
+        }
+    }
+}
