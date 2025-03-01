9d4705a1d18e4445ce9024900c7bbcbc135c98f6
Alexander Kharchenko <morontt@yandex.ru>
2025-03-01 11:12:57 +0300

    Add unit-tests

---
+++ tests/Service/TokenStorageTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * User: morontt
+ * Date: 01.03.2025
+ * Time: 10:23
+ */
+
+namespace Service;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use TeleBot\Repository\AccessTokenRepository;
+use TeleBot\Service\TokenStorage;
+
+#[CoversClass(TokenStorage::class)]
+class TokenStorageTest extends TestCase
+{
+    public function testEncryption(): void
+    {
+        $repositoryMock = $this->createMock(AccessTokenRepository::class);
+
+        $storage = new TokenStorage($repositoryMock, md5(time()));
+
+        $sourceString = 'Hello, Alice!';
+        $enc = $storage->encrypt($sourceString);
+
+        $this->assertSame($sourceString, $storage->decrypt($enc));
+    }
+}
