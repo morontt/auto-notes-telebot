b4edd0a141165206891a899a26615abc7756c2b2
Alexander Kharchenko <morontt@yandex.ru>
2025-03-07 19:34:01 +0300

    Currency symbols

--- src/Twig/Extension.php
+++ src/Twig/Extension.php
@@ -9,18 +9,41 @@
 namespace TeleBot\Twig;
 
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class Extension extends AbstractExtension
 {
+    /**
+     * Source: https://www.compart.com/en/unicode/category/Sc
+     */
+    private static array $currencyToCodePoints = [
+        'EUR' => 0x20AC,
+        'GEL' => 0x20BE,
+        'RUB' => 0x20BD,
+        'UAH' => 0x20B4,
+        'USD' => 0x24,
+    ];
+
+    private static array $currencyToString = [
+        'PLN' => 'zł',
+    ];
+
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('footer_date', [$this, 'footerDate']),
+            new TwigFunction('footer_date', [self::class, 'footerDate']),
         ];
     }
 
-    public function footerDate(): string
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('currency_symbol', [self::class, 'currencySymbol']),
+        ];
+    }
+
+    public static function footerDate(): string
     {
         $year = date('Y');
         if ($year === '2025') {
@@ -28,5 +51,18 @@ class Extension extends AbstractExtension
         }
 
         return '2025-' . $year;
+    }
+
+    public static function currencySymbol(string $code): string
+    {
+        if (!empty(self::$currencyToCodePoints[$code])) {
+            return mb_chr(self::$currencyToCodePoints[$code]);
+        }
+
+        if (!empty(self::$currencyToString[$code])) {
+            return self::$currencyToString[$code];
+        }
+
+        return $code;
     }
 }
