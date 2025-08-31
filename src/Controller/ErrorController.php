f8d5a78295c37a374169471a79e7960af4bd173e
Alexander Kharchenko <morontt@yandex.ru>
2025-08-31 16:14:55 +0300

    Error pages by NGINX

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * User: morontt
+ * Date: 31.08.2025
+ * Time: 13:17
+ */
+
+namespace TeleBot\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ErrorController extends AbstractController
+{
+    #[Route('/_errors/{code}', methods: ['GET'])]
+    public function renderAction(string $code): Response
+    {
+        if (!$this->container->has('twig')) {
+            throw new \LogicException('Twig is not available');
+        }
+
+        /** @var \Twig\Environment $twig */
+        $twig = $this->container->get('twig');
+        $template = sprintf('errors/error%s.html.twig', $code);
+        if (!$twig->getLoader()->exists($template)) {
+            $template = 'errors/error.html.twig';
+        }
+
+        return new Response($twig->render($template));
+    }
+}
