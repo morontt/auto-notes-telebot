a7427350307839ea03428b7a1206b3bfb0abfbef
Alexander Kharchenko <morontt@yandex.ru>
2025-09-30 10:04:37 +0300

    phpstan doctrine extension

---
+++ tests/object-manager.php
@@ -0,0 +1,13 @@
+<?php
+
+use Symfony\Component\Dotenv\Dotenv;
+use TeleBot\Kernel;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+(new Dotenv())->bootEnv(__DIR__ . '/../.env');
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel->boot();
+
+return $kernel->getContainer()->get('doctrine')->getManager();
