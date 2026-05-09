fe97457f11736ac526c6afd7877249f7786f1cc7
Alexander Kharchenko <morontt@yandex.ru>
2026-05-09 08:40:10 +0300

    Change deploy script

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -2,7 +2,8 @@
 
 composer install --optimize-autoloader --prefer-dist
 
-php bin/console doctrine:migrations:migrate --env=prod --no-interaction
+php bin/console doctrine:migrations:migrate --env=prod --no-interaction --configuration=config/migrations/autonotes.yaml
+php bin/console doctrine:migrations:migrate --env=prod --no-interaction --configuration=config/migrations/telebot.yaml
 rm -R ./var/cache/*
 php bin/console cache:warmup --env=prod
 
--- scripts/build_app_prod.sh
+++ scripts/build_app_prod.sh
@@ -10,7 +10,8 @@ cd /var/www/html || exit
 
 composer install --optimize-autoloader --prefer-dist
 
-php bin/console doctrine:migrations:migrate --env=prod -vvv --no-interaction
+php bin/console doctrine:migrations:migrate --env=prod -vvv --no-interaction --configuration=config/migrations/autonotes.yaml
+php bin/console doctrine:migrations:migrate --env=prod -vvv --no-interaction --configuration=config/migrations/telebot.yaml
 rm -R ./var/cache/*
 php bin/console cache:warmup --env=prod
 
